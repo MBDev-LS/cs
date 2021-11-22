ceec6b61dfd4c6e6e88f1f6f9562133332c3cbf1
LS <ltestevens@gmail.com>
2021-11-22 22:45:13 +0000

    Adds working simple decomposition.

--- linear-equation-solver/solve1.py
+++ linear-equation-solver/solve1.py
@@ -1,5 +1,6 @@
 import string
 
+OPERATORS = "+-"
 
 class EquationComponent():
     def __init__(self):
@@ -30,19 +31,98 @@ class Equation():
             return False
 
         if len(self.eq.split('=')[0]) == 0 or len(self.eq.split('=')[0]) == 0:
+            print("The must be at least one character on each side of the '=' sign.")
+            return False
+
         return True
 
     def decompose(self):
         self.left = self.eq.split('=')[0]
+        self.left_decomposed = []
         self.right = self.eq.split('=')[1]
+        self.right_decomposed = []
+
+        actual_count = 0
+
+        for i, char in enumerate(self.left): # Repeated code is a product of early testing, real version will store 'left' and 'right' in a list and iterate through it.
+            if not i < actual_count:
+                
+                if char in OPERATORS:
+                    self.left_decomposed.append(char)
+                elif char.isdigit():
+                    num = char
+                    c = 1
+                    if len(self.left)-1 >= i+c:
+                        while True and len(self.left)-1 >= i+c:
+                            # print(i+c, self.left)
+                            if self.left[i+c].isdigit() or self.left[i+c] == self.var:
+                                num += self.left[i+c]
+                            else:
+                                break
+                            c += 1
+                        actual_count = i + (c - 1)
+                    self.left_decomposed.append(num)
+                elif char == self.var:
+                    num = char
+                    c = 1
+                    if len(self.left)-1 >= i+c:
+                        while True and len(self.left)-1 >= i+c:
+                            # print(i+c, self.left)
+                            if self.left[i+c].isdigit() or self.left[i+c] == self.var:
+                                num += self.left[i+c]
+                            else:
+                                break
+                            c += 1
+                        actual_count = i + (c - 1)
+                    self.left_decomposed.append(num)
+
+                
+                actual_count += 1
+
+
+        actual_count = 0
+        for i, char in enumerate(self.right):
+            if not i < actual_count:
+                
+                if char in OPERATORS:
+                    self.right_decomposed.append(char)
+                elif char.isdigit():
+                    num = char
+                    c = 1
+                    if len(self.left)-1 >= i+c:
+                        while True and len(self.right)-1 >= i+c:
+                            # print(i+c, self.right)
+                            if self.right[i+c].isdigit() or self.right[i+c] == self.var:
+                                num += self.right[i+c]
+                            else:
+                                break
+                            c += 1
+                        actual_count = i + (c - 1)
+                    self.right_decomposed.append(num)
+                elif char == self.var:
+                    num = char
+                    c = 1
+                    if len(self.right)-1 >= i+c:
+                        while True and len(self.right)-1 >= i+c:
+                            # print(i+c, self.right)
+                            if self.right[i+c].isdigit() or self.right[i+c] == self.var:
+                                num += self.right[i+c]
+                            else:
+                                break
+                            c += 1
+                        actual_count = i + (c - 1)
+                    self.right_decomposed.append(num)
+
+                
+                actual_count += 1
         
-        if len(self.left) == 1
-        for i, char in self.left()
+        print(self.left_decomposed, self.right_decomposed)
+
 
     def __str__(self):
         return self.eq
 
 
-Eq = Equation("x=1+1", 'x')
+Eq = Equation("x=1+-11", 'x')
 Eq.verify()
 Eq.decompose()
