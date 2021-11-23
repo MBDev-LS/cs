1bf16de1ec38b2f0663deddc89f8a17cca3c32ce
LS <ltestevens@gmail.com>
2021-11-23 22:43:51 +0000

    Adds working shunting-yard algorithm, last commit for tonight.

--- linear-equation-solver/solve1.py
+++ linear-equation-solver/solve1.py
@@ -1,6 +1,6 @@
 import string
 
-OPERATORS = "+-"
+OPERATORS = "+-/*^()"
 
 
 class EquationComponent():
@@ -8,8 +8,9 @@ class EquationComponent():
         if component not in decomposed_sub_equation:
             return None
 
-        positions = [x for i, x in enumerate(decomposed_sub_equation) if x == component]
-        print(positions, decomposed_sub_equation)
+        positions = [x for i, x in enumerate(
+            decomposed_sub_equation) if x == component]
+        # print(positions, decomposed_sub_equation)
 
 
 class Equation():
@@ -95,7 +96,7 @@ class Equation():
                     c = 1
                     if len(self.right)-1 >= i+c:
                         while True and len(self.right)-1 >= i+c:
-                            print(i+c, self.right)
+                            # print(i+c, self.right)
                             if self.right[i+c].isdigit() or self.right[i+c] == self.var:
                                 num += self.right[i+c]
                             else:
@@ -109,7 +110,7 @@ class Equation():
                     if len(self.right)-1 >= i+c:
                         print("working")
                         while True and len(self.right)-1 >= i+c:
-                            print(i+c, self.right)
+                            # print(i+c, self.right)
                             if self.right[i+c].isdigit() or self.right[i+c] == self.var:
                                 print("working")
                                 num += self.right[i+c]
@@ -121,15 +122,70 @@ class Equation():
 
                 actual_count += 1
 
-        print(self.left_decomposed, self.right_decomposed)
+        return [self.left_decomposed, self.right_decomposed]
 
-        for item in self.right_decomposed:
-            subEq = EquationComponent(item, self.right_decomposed)
+        """for i, item in enumerate(self.right_decomposed):
+            # subEq = EquationComponent(item, self.right_decomposed)
+            print(item)
+            if item == '-':
+                if self.right_decomposed[i-1].isdigit():
+                    continue
+                elif self.right_decomposed[i-1] == '-':
+                    """
 
     def __str__(self):
         return self.eq
 
 
-Eq = Equation("x=1+-11", 'x')
+Eq = Equation("x=1/(2*2)+(2^4+4)/2", 'x')
 Eq.verify()
-Eq.decompose()
+decomosedEq = Eq.decompose()
+
+
+def lsplit(lst, item):
+    try:
+        index = lst.index(item)
+        return [lst[:index], lst[index+1:]]
+    except:
+        return [lst, lst]
+
+
+operators = {'+': 2, '-': 2, '/': 3, '*': 3, '^': 4}
+operators_associativity = {'+': 'left', '-': 'left', '/': 'left', '*': 'left', '^': 'right'}
+stack = []
+output = ''
+
+# print(decomosedEq)
+
+for comp in decomosedEq[1]:
+    # print(comp)
+    if comp == '(':
+        stack.append(comp)
+    elif comp == ')':
+        if len(stack) == 0:
+            continue
+        while stack[len(stack)-1] != '(': # Terrible way of doing this, what if there are missmatched brackets?
+            # print(stack[len(stack)-1], stack)
+            output += stack.pop() + ' '
+        if stack[len(stack)-1] == '(':
+            stack.pop()
+
+    elif not comp in list(operators.keys()):
+        output += comp + ' '
+    else:
+        if len(lsplit(stack, '(')[1]) > 0 and (operators[stack[len(stack)-1]] > operators[comp] or (operators[stack[len(stack)-1]] == operators[comp] and operators_associativity[comp] == 'left')):
+            output += stack.pop() + ' '
+        stack.append(comp)
+
+
+stack.reverse()
+
+for comp in stack:
+    if comp != '(':
+        output += comp + ' '
+
+
+# output += ''.join([comp for comp in stack if not '('])
+
+
+print(stack, output)
