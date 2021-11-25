a35002d3265b1dc69194ae5b3813917432b62bb0
Louis Stevens <louis@louisstevens.com>
2021-11-25 20:52:21 +0000

    End of class.

--- linear-equation-solver/project/notation.py
+++ linear-equation-solver/project/notation.py
@@ -3,104 +3,121 @@ import math
 
 
 def lsplit(lst, item):
-    try:
-        index = lst.index(item)
-        return [lst[:index], lst[index+1:]]
-    except:
-        return [lst, lst]
+	try:
+		index = lst.index(item)
+		return [lst[:index], lst[index+1:]]
+	except:
+		return [lst, lst]
 
 
 def isfloat(value: str):
-    try:
-        float(value)
-        return True
-    except:
-        return False
+	try:
+		float(value)
+		return True
+	except:
+		return False
 
 
 def infix_to_postfix(Eq):
 
-    decomosedEq = Eq.decompose()
-    results = []
+	decomosedEq = Eq.decompose()
+	results = []
 
-    for side in decomosedEq:
+	for side in decomosedEq:
 
-        stack = []
-        output = ''
+		stack = []
+		output = ''
 
-        for comp in side:
-            print(comp)
-            if comp == '(':
-                stack.append(comp)
-            elif comp == ')':
-                if len(stack) == 0:
-                    continue
-                while stack[len(stack)-1] != '(':
-                    output += stack.pop() + ' '
-                    if len(stack) == 0:
-                        print("Mismatching brackets.")
-                        exit()
-                if stack[len(stack)-1] == '(':
-                    stack.pop()
+		for i, comp in enumerate(side):
+			print(comp)
+			if comp == '(':
+				toeval = ''
+				c = 1
+				bracket_count = 1
+				while bracket_count > 0:
+					print(side, i+c)
+					current_comp = side.pop(i+1)
+					
+					if current_comp == '(':
+						bracket_count += 1
+					elif current_comp == ')':
+						bracket_count -= 1
+					
+					if bracket_count > 0:
+						toeval += current_comp
+					
+					print('e', toeval)
+					c += 1
+				stack.append(comp)
+			elif comp == ')':
+				if len(stack) == 0:
+					continue
+				while stack[len(stack)-1] != '(':
+					output += stack.pop() + ' '
+					if len(stack) == 0:
+						print("Mismatching brackets.")
+						exit()
+				if stack[len(stack)-1] == '(':
+					stack.pop()
 
-            elif not comp in list(OPERATORS.keys()):
-                output += comp + ' '
-            else:
-                if len(lsplit(stack, '(')[1]) > 0 and (OPERATORS[stack[len(stack)-1]] > OPERATORS[comp] or (OPERATORS[stack[len(stack)-1]] == OPERATORS[comp] and OPERATORS_ASSOCIATIVITY[comp] == 'left')):
-                    output += stack.pop() + ' '
-                stack.append(comp)
+			elif not comp in list(OPERATORS.keys()):
+				output += comp + ' '
+			else:
+				if len(lsplit(stack, '(')[1]) > 0 and (OPERATORS[stack[len(stack)-1]] > OPERATORS[comp] or (OPERATORS[stack[len(stack)-1]] == OPERATORS[comp] and OPERATORS_ASSOCIATIVITY[comp] == 'left')):
+					output += stack.pop() + ' '
+				stack.append(comp)
 
-        stack.reverse()
+		stack.reverse()
 
-        for comp in stack:
-            if comp != '(':
-                output += comp + ' '
+		for comp in stack:
+			if comp != '(':
+				output += comp + ' '
 
-        results.append(output)
+		results.append(output)
 
-    return results
+	return results
 
 
 def eval_postfix(expression: str, var):
-    expression = expression.replace(' ', '')
+	expression = expression.replace(' ', '')
 
-    stack = []
+	stack = []
 
-    for comp in expression:
-        print('Stack:', stack)
-        if comp in OPERATORS.keys():
-            var_in = False
-            operands = [stack.pop(), stack.pop()]
+	for comp in expression:
+		print('Stack:', stack)
+		if comp in OPERATORS.keys():
+			var_in = False
+			operands = [stack.pop(), stack.pop()]
 
-            for i, operand in enumerate(operands):
-                if var in operand:
-                    # print(operand)
-                    var_in = True
-                    if operand == var:
-                        operands[i] = 1
-                    else:
-                        factors = operand.split(var)
-                        for j, factor in enumerate(factors):
-                            if not isfloat(factor):
-                                factors.pop(j)
-                            else:
-                                if factor.isdigit():
-                                    factors[j] = int(factor)
-                                elif isfloat(factor):
-                                    factors[j] = float(factor)
+			for i, operand in enumerate(operands):
+				if var in operand:
+					# print(operand)
+					var_in = True
+					if operand == var:
+						operands[i] = 1
+					else:
+						factors = operand.split(var)
+						for j, factor in enumerate(factors):
+							if not isfloat(factor):
+								factors.pop(j)
+							else:
+								if factor.isdigit():
+									factors[j] = int(factor)
+								elif isfloat(factor):
+									factors[j] = float(factor)
 
-                        print('F', factors)
-                        operands[i] = str(math.prod(factors))
+						print('F', factors)
+						operands[i] = str(math.prod(factors))
 
-            calc = f"{operands[1]} {comp} {operands[0]}".replace('^', '**')
-            print(calc)
-            # print(calc)
-            res = eval(calc)
-            stack.append(str(res) + 'x' if var_in else str(res))
-        else:
-            stack.append(str(comp))
-    # print(stack)
-    return stack[0]
+			calc = f"{operands[1]} {comp} {operands[0]}".replace('^', '**')
+			print(calc)
+			# print(calc)
+			res = eval(calc)
+			stack.append(str(res) + 'x' if var_in else str(res))
+		else:
+			stack.append(str(comp))
+	# print(stack)
+	return stack[0]
 
 
 # print(eval_postfix("1 2 2 * x * 2 * / 2 4 ^ 4 + 2 / + ", 'x'))
--- linear-equation-solver/project/objects.py
+++ linear-equation-solver/project/objects.py
@@ -3,139 +3,139 @@ import string
 from config import OPERATORS_STRING, OPERATORS, OPERATORS_ASSOCIATIVITY
 
 def lsplit(lst, item):
-    try:
-        index = lst.index(item)
-        return [lst[:index], lst[index+1:]]
-    except:
-        return [lst, lst]
+	try:
+		index = lst.index(item)
+		return [lst[:index], lst[index+1:]]
+	except:
+		return [lst, lst]
 
 class Equation():
-    def __init__(self, equation: str, var: str):
+	def __init__(self, equation: str, var: str):
 
-        self.eq = equation
-        self.var = var
+		self.eq = equation
+		self.var = var
 
-    def verify(self):
-        chars = []
-        for c in self.eq:
-            if not c.isdigit() and c in string.ascii_lowercase:
-                chars.append(c)
-        if len(list(set(chars))) > 1:
-            print(
-                'This program currently only supports equations with one unknown variable')
-            return False
+	def verify(self):
+		chars = []
+		for c in self.eq:
+			if not c.isdigit() and c in string.ascii_lowercase:
+				chars.append(c)
+		if len(list(set(chars))) > 1:
+			print(
+				'This program currently only supports equations with one unknown variable')
+			return False
 
-        if self.eq.count('=') < 1:
-            print("The equation must have one = sign.")
-            return False
-        elif self.eq.count('=') > 1:
-            print("The equation must have only one = sign.")
-            return False
+		if self.eq.count('=') < 1:
+			print("The equation must have one = sign.")
+			return False
+		elif self.eq.count('=') > 1:
+			print("The equation must have only one = sign.")
+			return False
 
-        if len(self.eq.split('=')[0]) == 0 or len(self.eq.split('=')[0]) == 0:
-            print("The must be at least one character on each side of the '=' sign.")
-            return False
+		if len(self.eq.split('=')[0]) == 0 or len(self.eq.split('=')[0]) == 0:
+			print("The must be at least one character on each side of the '=' sign.")
+			return False
 
-        return True
+		return True
 
-    def decompose(self):
-        self.left = self.eq.split('=')[0]
-        self.left_decomposed = []
-        self.right = self.eq.split('=')[1]
-        self.right_decomposed = []
+	def decompose(self):
+		self.left = self.eq.split('=')[0]
+		self.left_decomposed = []
+		self.right = self.eq.split('=')[1]
+		self.right_decomposed = []
 
-        actual_count = 0
+		actual_count = 0
 
-        # Repeated code is a product of early testing, real version will store 'left' and 'right' in a list and iterate through it.
-        for i, char in enumerate(self.left):
-            if not i < actual_count:
+		# Repeated code is a product of early testing, real version will store 'left' and 'right' in a list and iterate through it.
+		for i, char in enumerate(self.left):
+			if not i < actual_count:
 
-                if char in OPERATORS_STRING:
-                    self.left_decomposed.append(char)
-                elif char.isdigit():
-                    num = char
-                    c = 1
-                    if len(self.left)-1 >= i+c:
-                        while True and len(self.left)-1 >= i+c:
-                            # print(i+c, self.left)
-                            if self.left[i+c].isdigit() or self.left[i+c] == self.var:
-                                num += self.left[i+c]
-                            else:
-                                break
-                            c += 1
-                        actual_count = i + (c - 1)
-                    self.left_decomposed.append(num)
-                elif char == self.var:
-                    num = char
-                    c = 1
-                    if len(self.left)-1 >= i+c:
-                        while True and len(self.left)-1 >= i+c:
-                            # print(i+c, self.left)
-                            if self.left[i+c].isdigit() or self.left[i+c] == self.var:
-                                num += self.left[i+c]
-                            else:
-                                break
-                            c += 1
-                        actual_count = i + (c - 1)
-                    self.left_decomposed.append(num)
+				if char in OPERATORS_STRING:
+					self.left_decomposed.append(char)
+				elif char.isdigit():
+					num = char
+					c = 1
+					if len(self.left)-1 >= i+c:
+						while True and len(self.left)-1 >= i+c:
+							# print(i+c, self.left)
+							if self.left[i+c].isdigit() or self.left[i+c] == self.var:
+								num += self.left[i+c]
+							else:
+								break
+							c += 1
+						actual_count = i + (c - 1)
+					self.left_decomposed.append(num)
+				elif char == self.var:
+					num = char
+					c = 1
+					if len(self.left)-1 >= i+c:
+						while True and len(self.left)-1 >= i+c:
+							# print(i+c, self.left)
+							if self.left[i+c].isdigit() or self.left[i+c] == self.var:
+								num += self.left[i+c]
+							else:
+								break
+							c += 1
+						actual_count = i + (c - 1)
+					self.left_decomposed.append(num)
 
-                actual_count += 1
+				actual_count += 1
 
-        actual_count = 0
-        for i, char in enumerate(self.right):
-            if not i < actual_count:
+		actual_count = 0
+		for i, char in enumerate(self.right):
+			if not i < actual_count:
 
-                if char in OPERATORS_STRING:
-                    self.right_decomposed.append(char)
-                elif char.isdigit():
-                    num = char
-                    c = 1
-                    if len(self.right)-1 >= i+c:
-                        while True and len(self.right)-1 >= i+c:
-                            # print(i+c, self.right)
-                            if self.right[i+c].isdigit() or self.right[i+c] == self.var:
-                                num += self.right[i+c]
-                            else:
-                                break
-                            c += 1
-                        actual_count = i + (c - 1)
-                    self.right_decomposed.append(num)
-                elif char == self.var:
-                    num = char
-                    c = 1
-                    if len(self.right)-1 >= i+c:
-                        print("working")
-                        while True and len(self.right)-1 >= i+c:
-                            if self.right[i+c].isdigit() or self.right[i+c] == self.var:
-                                print("working")
-                                num += self.right[i+c]
-                            else:
-                                break
-                            c += 1
-                        actual_count = i + (c - 1)
-                    self.right_decomposed.append(num)
+				if char in OPERATORS_STRING:
+					self.right_decomposed.append(char)
+				elif char.isdigit():
+					num = char
+					c = 1
+					if len(self.right)-1 >= i+c:
+						while True and len(self.right)-1 >= i+c:
+							# print(i+c, self.right)
+							if self.right[i+c].isdigit() or self.right[i+c] == self.var:
+								num += self.right[i+c]
+							else:
+								break
+							c += 1
+						actual_count = i + (c - 1)
+					self.right_decomposed.append(num)
+				elif char == self.var:
+					num = char
+					c = 1
+					if len(self.right)-1 >= i+c:
+						print("working")
+						while True and len(self.right)-1 >= i+c:
+							if self.right[i+c].isdigit() or self.right[i+c] == self.var:
+								print("working")
+								num += self.right[i+c]
+							else:
+								break
+							c += 1
+						actual_count = i + (c - 1)
+					self.right_decomposed.append(num)
 
-                actual_count += 1
-        
-        for comp in self.left_decomposed:
-            if self.var in comp:
-                factors = [char for char in comp]
-                for i in range(1, len(factors)):
-                    factors.insert(i, '*')
-                newlistparts = lsplit(self.left_decomposed, comp)
-                newlist = newlistparts[0] + ['('] + factors + [')'] +  newlistparts[1]
-                self.left_decomposed = newlist
+				actual_count += 1
+		
+		for comp in self.left_decomposed:
+			if self.var in comp:
+				factors = [char for char in comp]
+				for i in range(1, len(factors)):
+					factors.insert(i, '*')
+				newlistparts = lsplit(self.left_decomposed, comp)
+				newlist = newlistparts[0] + ['('] + factors + [')'] +  newlistparts[1]
+				self.left_decomposed = newlist
 
-        for comp in self.right_decomposed:
-            if self.var in comp:
-                factors = [char for char in comp]
-                for i in range(1, len(factors)):
-                    factors.insert(i, '*')
-                newlistparts = lsplit(self.right_decomposed, comp)
-                newlist = newlistparts[0] + ['('] + factors + [')'] +  newlistparts[1]
-                self.right_decomposed = newlist
+		for comp in self.right_decomposed:
+			if self.var in comp:
+				factors = [char for char in comp]
+				for i in range(1, len(factors)):
+					factors.insert(i, '*')
+				newlistparts = lsplit(self.right_decomposed, comp)
+				newlist = newlistparts[0] + ['('] + factors + [')'] +  newlistparts[1]
+				self.right_decomposed = newlist
 
-        return [self.left_decomposed, self.right_decomposed]
+		return [self.left_decomposed, self.right_decomposed]
 
-    def __str__(self):
-        return self.eq
+	def __str__(self):
+		return self.eq
