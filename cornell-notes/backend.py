6c0fab1d3a91f3d888468d4a25f7da35addab93c
Louis Stevens <louis@louisstevens.com>
2021-11-30 11:53:15 +0000

    cd

--- cornell-notes/backend.py
+++ cornell-notes/backend.py
@@ -5,11 +5,23 @@ BASE_DIR = Path(__file__).resolve().parent
 NOTE_DIR = BASE_DIR / 'notes'
 
 def getSubjects():
-	directoryContents = os.listdir(NOTE_DIR)
+	directoryContents = os.listdir(NOTE_DIR/'e')
 	return directoryContents
 
 def getTopics(subject):
-	pass
+	try:
+		directoryContents = os.listdir(NOTE_DIR / subject)
+		return {"success": True, "topics": [file[:-5] for file in directoryContents]}
+	except FileNotFoundError:
+		try:
+			os.listdir(NOTE_DIR)
+		except:
+			return {"success": False, "error": "the notes directory cannot be found"}
+		else:
+			return {"success": False, "error": "this subject does not exist"}
 
 def loadTopic(subject,topic):
 	pass
+
+getSubjects()
+print(getTopics("Subjecet One"))
