de6d252f26a2266f7b342e1a9ae10eebe8d775d5
Louis Stevens <louis@louisstevens.com>
2022-01-19 14:03:24 +0000

    Adds seemingly finished pre_flop method for the Table class.

--- OOP/lesson2/polka.py
+++ OOP/lesson2/polka.py
@@ -3,72 +3,102 @@ import random
 
 class Card():
 
-    RANKNAME = ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+	RANKNAME = ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
 
-    def __init__(self, suit: str, rank: int) -> None:
-        self._suit = suit
-        self._rank = rank
-    
-    def get_suit(self):
-        return self._suit
+	def __init__(self, suit: str, rank: int) -> None:
+		self._suit = suit
+		self._rank = rank
+	
+	def get_suit(self):
+		return self._suit
 
-    def get_rank(self):
-        return self._rank
-    
-    def __str__(self) -> str:
-        return Card.RANKNAME[self._rank] + self._suit[0]
+	def get_rank(self):
+		return self._rank
+	
+	def __str__(self) -> str:
+		return Card.RANKNAME[self._rank] + self._suit[0]
 
 
 class Deck():
-    def __init__(self) -> None:
-        self.cards = []
+	def __init__(self) -> None:
+		self.cards = []
 
-        for suit in ['Hearts', 'Clubs', 'Diamonds', 'Spades']:
-            for num_rank in range(1, 14):
-                self.cards.append(Card(suit, num_rank))
-    
-    def shuffle(self) -> None:
-        random.shuffle(self.cards)
+		for suit in ['Hearts', 'Clubs', 'Diamonds', 'Spades']:
+			for num_rank in range(1, 14):
+				self.cards.append(Card(suit, num_rank))
+	
+	def shuffle(self) -> None:
+		random.shuffle(self.cards)
 
-    def take_card(self) -> Card:
-        return self.cards.pop()
+	def take_card(self) -> Card:
+		return self.cards.pop()
 
-    def __str__(self) -> str:
-        return f'<Deck: {", ".join([str(card) for card in self.cards])}>'
+	def __str__(self) -> str:
+		return f'<Deck: {", ".join([str(card) for card in self.cards])}>'
 
 class Player():
-    def __init__(self, name) -> None:
-        self.name = name
-        self.cards = []
+	def __init__(self, name, startingBalance) -> None:
+		self.name = name
+		self.cards = []
+		self.balance = startingBalance
+		self.bet = 0
 
-    def set_name(self, name) -> None:
-        name = name
-    
-    def get_name(self) -> str:
-        self.name
-    
-    def get_cards(self) -> list:
-        return self.cards
-    
-    def __str__(self) -> str:
-        return f'<Player: {self.name}, {", ".join([str(card) for card in self.cards])}'
+	def set_name(self, name) -> None:
+		name = name
+	
+	def get_name(self) -> str:
+		self.name
+	
+	def get_cards(self) -> list:
+		return self.cards
+	
+	def __str__(self) -> str:
+		return f'<Player: {self.name}, {", ".join([str(card) for card in self.cards])}>'
 
 class Table():
-    def __init__(self, *args) -> None:
-        self.deck = Deck()
-        self.players = []
-        for name in args:
-            if isinstance(name, str):
-                self.players.append(Player(name))
-    
-    def begin_game(self):
-        self.deck.shuffle()
-        for player in self.players:
-            player.cards.extend([self.deck.take_card(), self.deck.take_card()])
+	def __init__(self, startingBalance, *args) -> None:
+		self.deck = Deck()
+		self.players = []
+		self.totalBet = 0
 
-    def __str__(self) -> str:
-        return f'<Table: (deck: {self.deck}) (players: {", ".join([str(player) for player in self.players])})>'
+		for name in args:
+			if isinstance(name, str):
+				self.players.append(Player(name, startingBalance))
 
-table = Table('deck', 'Jeff', 'Dave', 'Sally', 'Matt')
-table.begin_game()
+		self.big_blind = 10
+		self.small_blind = 5
+	
+	def take_bet(self, player, amount):
+		if player not in self.players:
+			raise '[ERROR] Attempted to place bet for player not in game.'
+		elif player.balance < amount:
+			print(f'{player.name} attempted to bet more than their balance of {player.balance}.')
+		
+		player.balance -= amount 
+		player.bet += amount
+		self.totalBet += amount
+
+		print(f'[BET] {player.name} bet {amount}, leaving them with a balance of {player.balance} and a total bet of {player.bet}')
+
+	
+	def pre_flop(self):
+		self.deck.shuffle()
+		for i, player in enumerate(self.players):
+			player.cards.extend([self.deck.take_card(), self.deck.take_card()])
+			print(f'[CARD TRACKING] {player.name} has the cards: {", ".join([str(card) for card in player.cards])}')
+			if i == 0:
+				print(f'[BLINDS] {player.name} is the big blind.')
+				self.take_bet(player, self.big_blind)
+			elif i == 1:
+				print(f'[BLINDS] {player.name} is the small blind.')
+				self.take_bet(player, self.small_blind)
+
+	def flop(self):
+		pass
+	
+	def __str__(self) -> str:
+		return f'<Table: (deck: {self.deck}) (players: {", ".join([str(player) for player in self.players])})>'
+
+table = Table(100, 'Jeff', 'Dave', 'Sally', 'Matt')
+table.pre_flop()
 print(table)
