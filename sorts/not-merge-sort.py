827eaf0e353b3e5661b4331c7dd91720c9904f15
Louis Stevens <louis@louisstevens.com>
2022-02-24 17:06:04 +0000

    Adds working not merge sort.

--- sorts/not-merge-sort.py
+++ sorts/not-merge-sort.py
@@ -1,18 +1,27 @@
 import copy
+import random
 
 lst = [1, 7, 3, 1]
+lst = [1, 7, 3, 1, 7, 4, 9, 100, 6, 2, 8, 1]
+lst = [random.randint(1, 1000) for i in range(1001)]
+print(lst)
 
 sortingList = [[item] for item in lst]
 newSortingList = copy.deepcopy(sortingList)
 
 while len(sortingList) > 1:
-	print("Started new loop.")
+	# print("Started new loop.")
 	sortingList = copy.deepcopy(newSortingList)
+
 	if len(sortingList) == 1:
 		break
 
-	for i in range(0, len(sortingList), 2):
-		print("Started step")
+	for i in range(0, len(sortingList)):
+		if i >= len(newSortingList)-1:
+			continue
+
+		# print('LOOK HERE:', len(sortingList))
+		# print("Started step")
 		listA = newSortingList.pop(i)
 		taken = False
 		if len(newSortingList)-1 < i:
@@ -26,7 +35,7 @@ while len(sortingList) > 1:
 		newList = []
 
 		while len(listA) > 0 and len(listB) > 0:
-			print("Ran mini sort")
+			# print("Ran mini sort")
 			if listA[0] < listB[0]:
 				newList.append(listA.pop(0))
 			else:
@@ -38,5 +47,8 @@ while len(sortingList) > 1:
 
 		newSortingList.insert(i, newList)
 
-print(sortingList)
 
+lst.sort()
+print(lst)
+print(sortingList[0])
+print(lst == sortingList[0])
