5dff563d66ea7e4fa7d81e4978dab3ebba35465d
LS <ltestevens@gmail.com>
2022-03-10 21:08:58 +0000

    Makes rgexToFsm a function for recursion.

--- topics/finite-state-machines/regex-to-fsm.py
+++ topics/finite-state-machines/regex-to-fsm.py
@@ -1,50 +1,82 @@
 
-from ast import operator
 from pprint import pprint
-
-
-
-regexString = r'ab+'
 
 def getInitialState(machine: dict) -> str:
 	for stateKey in machine:
-		if machine[stateKey]['initial_state']:
+		if machine[stateKey]['meta_data']['initial_state']:
 			return stateKey
 
-machine = {
-	'S1': {
-		'accept_state': False,
-		'initial_state': True,
-	}
-}
 
-currentState = getInitialState(machine)
-stateCount = 2
 
 def zeroOrMore(char, machine, stateCount):
-	machine[f'S{stateCount-1}'][char] = f'S{stateCount-1}'
+	machine[f'S{stateCount-1}']['transitions'][char] = f'S{stateCount-1}'
 
 	return machine
 
 def OneOrMore(char, machine, stateCount):
-	machine[f'S{stateCount-1}'][char] = f'S{stateCount}'
+	machine[f'S{stateCount-1}']['transitions'][char] = f'S{stateCount}'
 
 	machine[f'S{stateCount}'] = {
-		'accept_state': False,
-		'initial_state': False,
+		'meta_data': {
+			'accept_state': False,
+			'initial_state': False,
+		},
+		'transitions': {}
 	}
 
-	machine[f'S{stateCount}'][char] = f'S{stateCount}'
+	machine[f'S{stateCount}']['transitions'][char] = f'S{stateCount}'
 
 	return machine
 
 def addRegularState(char, currentState, stateCount, machine):
-	machine[currentState][char] = f'S{stateCount}'
+	machine[currentState]['transitions'][char] = f'S{stateCount}'
 		
 	machine[f'S{stateCount}'] = {
-		'accept_state': False,
-		'initial_state': False,
+		'meta_data': {
+			'accept_state': False,
+			'initial_state': False,
+		},
+		'transitions': {}
 	}
+
+	return machine
+
+def regexToFsm(regexString, operators):
+	machine = {
+	'S1': {
+		'meta_data': {
+			'accept_state': False,
+			'initial_state': True,
+		},
+		'transitions': {}
+	}
+}
+
+	currentState = getInitialState(machine)
+	stateCount = 2
+
+	for i, char in enumerate(regexString):
+		stateCreated = False
+		if char in operators:
+			continue
+
+		if i == len(regexString)-1:
+			machine = addRegularState(char, currentState, stateCount, machine)
+			stateCreated = True
+
+			continue
+
+		if regexString[i+1] in operators:
+			machine = operators[regexString[i+1]]['func'](char, machine, stateCount)
+			stateCreated = operators[regexString[i+1]]['stateCreated']
+		else:
+			machine = addRegularState(char, currentState, stateCount, machine)
+			stateCreated = True
+
+		currentState = f'S{stateCount}'
+		stateCount += 1 if stateCreated is True else 0
+
+	machine[f'S{stateCount-1}']['meta_data']['accept_state'] = True
 
 	return machine
 
@@ -60,27 +92,8 @@ operators = {
 
 }
 
-for i, char in enumerate(regexString):
-	stateCreated = False
-	if char in operators:
-		continue
+regexString = r'ab+'
 
-	if i == len(regexString)-1:
-		machine = addRegularState(char, currentState, stateCount, machine)
-		stateCreated = True
+finalMachine = regexToFsm(regexString, operators)
 
-		continue
-
-	if regexString[i+1] in operators:
-		machine = operators[regexString[i+1]]['func'](char, machine, stateCount)
-		stateCreated = operators[regexString[i+1]]['stateCreated']
-	else:
-		machine = addRegularState(char, currentState, stateCount, machine)
-		stateCreated = True
-
-	currentState = f'S{stateCount}'
-	stateCount += 1 if stateCreated is True else 0
-
-machine[f'S{stateCount-1}']['accept_state'] = True
-
-pprint(machine)
+pprint(finalMachine)
