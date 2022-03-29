83acddc405d05304bed3daaf0e9269f075809356
Louis Stevens <louis@louisstevens.com>
2022-03-29 08:54:25 +0100

    Adds seemingly working circular queue.

--- topics/data-structures/queues/func-circular-queue.py
+++ topics/data-structures/queues/func-circular-queue.py
@@ -3,42 +3,71 @@ def createQueue(queueLength: int) -> tuple:
     queue = [None for i in range(queueLength)]
     front = 0
     rear = -1
+    size = 0
     queueMaxLength = len(queue)
     
-    return queue, front, rear, queueMaxLength
+    return queue, front, rear, size, queueMaxLength
 
-def isFull(front: int, rear: int, queueMaxLength: int) -> bool:
-    size = rear - front + 1
-    if size >= queueMaxLength:
+def isFull(size: int, queueMaxLength: int) -> bool:
+    if size == queueMaxLength:
         return True
     
     return False
 
-def isEmpty(front: int, rear: int) -> bool:
-    size = rear - front
-    if size < 0:
+def isEmpty(size: int) -> bool:
+    if size <= 0:
         return True
     
     return False
 
-def enQueue(queue: list, front: int, rear: int, queueMaxLength: int, itemToEnQueue) -> tuple:
-    if isFull(front, rear, queueMaxLength) is True:
+def enQueue(queue: list, front: int, rear: int, size: int, queueMaxLength: int, itemToEnQueue) -> tuple:
+    if isFull(size, queueMaxLength) is True:
         print(f'Failed to enqueue {itemToEnQueue} as queue ({", ".join(queue)}) is full.')
-        return
+        return queue, rear, size
     
-    rear += 1
+    rear = (rear + 1) % queueMaxLength
     queue[rear] = itemToEnQueue
+    size += 1
 
-    return queue, rear
+    return queue, rear, size
 
-def deQueue(queue: list, front: int, rear: int, queueMaxLength: int) -> tuple:
-    if isEmpty(front, rear) is True:
+def deQueue(queue: list, front: int, rear: int, size: int, queueMaxLength: int) -> tuple:
+    if isEmpty(size) is True:
         print(f'Failed to dequeue from queue, as queue is empty.')
         return -1, queue, front
     
     deQueuedItem = queue[front]
     queue[front] = None
     
-    front += 1
+    front = (front + 1) % queueMaxLength
+    size -= 1
 
-    return deQueuedItem, queue, front
+    return deQueuedItem, queue, front, size
+
+def logQueue(queue: list, front: int, rear: int, size: int, queueMaxLength: int):
+    print(f'Queue: {queue}')
+    print(f'isEmpty: {isEmpty(size)}')
+    print(f'isFull: {isFull(size, queueMaxLength)}')
+
+queue, front, rear, size, queueMaxLength = createQueue(4)
+
+logQueue(queue, front, rear, size, queueMaxLength)
+
+queue, rear, size = enQueue(queue, front, rear, size, queueMaxLength, 'Chris')
+
+logQueue(queue, front, rear, size, queueMaxLength)
+
+queue, rear, size = enQueue(queue, front, rear, size, queueMaxLength, 'Alan')
+queue, rear, size = enQueue(queue, front, rear, size, queueMaxLength, 'Peter')
+queue, rear, size = enQueue(queue, front, rear, size, queueMaxLength, 'Sylvester')
+
+logQueue(queue, front, rear, size, queueMaxLength)
+
+deQueuedItem, queue, front, size = deQueue(queue, front, rear, size, queueMaxLength)
+
+logQueue(queue, front, rear, size, queueMaxLength)
+
+queue, rear, size = enQueue(queue, front, rear, size, queueMaxLength, 'Douglas')
+queue, rear, size = enQueue(queue, front, rear, size, queueMaxLength, 'Donald')
+
+logQueue(queue, front, rear, size, queueMaxLength)
