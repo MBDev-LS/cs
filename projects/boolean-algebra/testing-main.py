268bf73fce5efd1f8db272a89e0ee13c18a9b6da
Louis Stevens <louis@louisstevens.com>
2022-03-31 16:28:23 +0100

    Adds complex working result.

--- projects/boolean-algebra/testing-main.py
+++ projects/boolean-algebra/testing-main.py
@@ -3,7 +3,7 @@ import sya_in_func
 import postFix_to_tree
 
 
-expression = '(A+B).(C+D)'
+expression = 'A.B.C+A.C+A.C.D+A.C.~D'
 postFixWithSpaces = sya_in_func.booleanInFixToPostFix(expression)
 tree = postFix_to_tree.postfixToTree(postFixWithSpaces)
 
