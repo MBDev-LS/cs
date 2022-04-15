2fefc176262826ff88c71bc79f7caa4d9a7b9afa
LS <ltestevens@gmail.com>
2022-04-15 23:44:58 +0100

    Adds non-working regex tree.

--- projects/boolean-algebra/simplification/regex_tree.py
+++ projects/boolean-algebra/simplification/regex_tree.py
@@ -1,4 +1,5 @@
 
+import re
 from pprint import pprint
 
 from identities import identitiesDict
@@ -8,4 +9,27 @@ expression = 'A+(B.C)'
 
 dynamicIdentList = dynamic_indentities.generate_dynamic_identities_list(expression, identitiesDict)
 
-pprint(dynamicIdentList)
+# pprint(dynamicIdentList)
+
+def recursive_simplification(expression, dynamicIdentList, simplificationHistory=None):
+	simplificationHistory = simplificationHistory if simplificationHistory is not None else [expression]
+	histories = [simplificationHistory]
+
+	for identity in dynamicIdentList:
+		matchObject = re.match(identity['startRegex'], expression)
+		if matchObject is None:
+			continue
+
+		newExpression = re.sub(identity['startRegex'], identity['resultRegex'].replace('\\', ''), expression)
+
+		if newExpression in simplificationHistory:
+			continue
+
+		simplificationHistory.append(newExpression)
+
+		exendedHistorys = recursive_simplification(newExpression, dynamicIdentList, simplificationHistory)
+		histories += exendedHistorys
+	
+	return histories
+
+print(recursive_simplification(expression, dynamicIdentList))
