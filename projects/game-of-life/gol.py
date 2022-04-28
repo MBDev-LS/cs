2ae4a8134e65319827fe5d312f94c5925ecd5f77
Louis Stevens <louis@louisstevens.com>
2022-04-28 14:49:46 +0100

    Adds gol.

--- projects/game-of-life/gol.py
+++ projects/game-of-life/gol.py
@@ -1,8 +1,10 @@
 
 import copy
 
-xDimension = 10
-yDimension = 20
+from sqlalchemy import true
+
+xDimension = 40
+yDimension = 30
 
 boardList = [[0 for x in range(xDimension)] for y in range(yDimension)]
 
@@ -11,16 +13,18 @@ boardList = [[0 for x in range(xDimension)] for y in range(yDimension)]
 #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 #     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+#     [0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
 #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 # ]
 
+# boardList = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+print(boardList)
 
-cycles = 10
+cycles = 100
 
 def updateCell(cellCoordX: int, cellCoordY: int, board: list) -> int:
 	surroundingCells = []
@@ -62,15 +66,83 @@ def updateCell(cellCoordX: int, cellCoordY: int, board: list) -> int:
 	return 0
 
 def displayBoard(board):
-	print('_'*(2+len(board[0])))
 
-	for y in range(len(board)):
-		# print('|' + ''.join(["⬛️" if item == 1 else "⬜️" for item in board[y]] ) + '|')
-		print('|' + ''.join(["■" if item == 1 else "□" for item in board[y]] ) + '|')
+	maxLength = len(str(len(board[0])))
+	lstOfLines = [[] for i in range(maxLength)]
+
+
+	for i, lst in enumerate(lstOfLines):
+		for y in range(len(board)):
+			if len(str(y)) > i:
+				lst.append(str(y)[i])
+			else:
+				lst.append(' ')
 	
-	print('‾'*(2+len(board[0])))
+	for lst in lstOfLines:
+		print(' '*5+' '.join(lst))
+
+
+	# print('     '+''.join([str(i) + ' ' for i in range(len(board[0]))]))
+	print('    ' + '_'*int(2+2*len(board[0])))
+
+	
+	for y in range(len(board)):
+
+		print(f'{y:>3} |' + ''.join(["⬛️" if item == 1 else "⬜️" for item in board[y]] ) + '|')
+		# print('|' + ''.join(["■" if item == 1 else "□" for item in board[y]] ) + '|')
+	
+	print('    '+'‾'*int(2+2*len(board[0])))
+
+def intInput(prompt: str) -> int:
+	userInput = input(prompt)
+	while userInput.isdigit() is not True:
+		if userInput == 'e':
+			return -1
+		
+		print('Enter an integer.')
+		userInput = input(prompt)
+
+	
+	return int(userInput)
+
 
 displayBoard(boardList)
+
+while True:
+	xToEdit = intInput('Enter X coord to edit (enter \'e\' to abandon editing): ')
+	if xToEdit == -1:
+		print('Editing stopped.')
+		break
+	elif xToEdit >= xDimension:
+		print('That coordiate is out of the valid range.')
+		continue
+	
+
+	yToEdit = intInput('Enter Y coord to edit (enter \'e\' to abandon editing): ')
+	if yToEdit == -1:
+		print('Editing stopped.')
+		break
+	elif yToEdit >= yDimension:
+		print('That coordiate is out of the valid range.')
+		continue
+
+	value = input('Set this coord to alive (1) or dead (0): ')
+	while value not in ['alive', '0', 'dead', '1']:
+		if value == -1:
+			print('Editing stopped.')
+			break
+
+		print('Invalid response.')
+		value = input('Set this coord to alive (1) or dead (0): ')
+	
+	boardList[yToEdit][xToEdit] = 1 if value in ['alive', '1'] else 0
+
+	display = input('Display board (y/n): ').lower()
+	while display not in ['y', 'n']:
+		display = input('Display board (y/n): ').lower()
+	
+	if display == 'y':
+		displayBoard(boardList)
 
 for cycleCount in range(cycles):
 	newBoard = [[0 for x in range(xDimension)] for y in range(yDimension)]
@@ -88,6 +160,8 @@ for cycleCount in range(cycles):
 			flat.append(boardList[y][x])
 
 	displayBoard(boardList)
+
+	input()
 
 	if len(set(flat)) == 1 and boardList[0][0] == 0:
 		print('Everything is dead.')
