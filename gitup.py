bccd3c99168c0ba30480f1ad21a2b5eac9080263
Louis Stevens <louis@louisstevens.com>
2022-05-20 09:39:53 +0100

    Fixes gitup

--- gitup.py
+++ gitup.py
@@ -6,14 +6,15 @@ def main():
 	import os
 
 	arguments = sys.argv
-	if len(arguments) == 0:
+	print(arguments)
+	if len(arguments) == 1:
 		print('error: please provide a commit reason')
 		exit()
-	if len(arguments) > 1:
+	if len(arguments) > 2:
 		print(f'error: expected 1 argument, {len(arguments)} given')
 		exit()
 
-	os.system(f'git pull; git add --all; git commit -m "{arguments[0]}"; git push')
+	os.system(f'git pull; git add --all; git commit -m "{arguments[1]}"; git push')
 
 
 if __name__ == '__main__':
