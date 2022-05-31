6adfb273e2fca9adc05427a6ecc23049dda9f398
Louis Stevens <louis@louisstevens.com>
2022-05-31 18:43:58 +0100

    Removes last batch of MPs.

--- general-programming-practice/mp-spreadsheet-too-quick.py
+++ general-programming-practice/mp-spreadsheet-too-quick.py
@@ -3,17 +3,7 @@ import pyperclip
 
 output = ''
 
-mps = """Paul Holmes (Eastleigh)
-Alicia Kearns (Rutland and Melton)
-Sir Robert Syms (Poole)
-Jeremy Wright (Kenilworth and Southam)
-Elliot Colburn (Carshalton and Wallington)
-Nickie Aiken (Cities of London & Westminster)
-Dr Dan Poulter (Central Suffolk and N Ipswich)
-Andrew Bridgen (NW Leicestershire)
-Tom Tugendhat (Tonbridge & Malling)
-Andrea Leadsom (South Northamptonshire)
-John Stevenson (Carlisle)""".split('\n')
+mps = """""".split('\n')
 
 print(mps)
 
