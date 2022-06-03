8dc030dbf38fbd5263bebf7ff5a5477d7d574ea0
LS <ltestevens@gmail.com>
2022-06-03 22:33:24 +0100

    Adds basis for ams program.

---
+++ politics/voting-system/additional-member-system.py
@@ -0,0 +1,20 @@
+
+mockDatabase = {
+    "topup_votes": [
+        {
+            "party_name": "Spotty Party",
+            "votes": 0,
+            "count": {
+                "area_seats": 0,
+                "dhondt": {
+                    "dseats": 0,
+                    
+                }
+            }
+        }
+    ]
+}
+
+def getVotes(candidateList: list):
+    print('You are able ')
+
