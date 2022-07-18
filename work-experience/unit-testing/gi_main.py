57a470c9f90f4bb22736b70b5939ff2e3f09cb5a
LS <ltestevens@gmail.com>
2022-07-18 12:50:46 +0100

    Adds first two tests

---
+++ work-experience/unit-testing/gi_main.py
@@ -0,0 +1,20 @@
+
+import gi_tests
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+
+
+gi_tests.unittest.main()
