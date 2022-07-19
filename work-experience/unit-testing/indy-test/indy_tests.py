f07786e734cad9288081d8209b1e62951ee50620
LS <ltestevens@gmail.com>
2022-07-19 12:33:01 +0100

    Adds documentation to indy_tests.py

--- work-experience/unit-testing/indy-test/indy_tests.py
+++ work-experience/unit-testing/indy-test/indy_tests.py
@@ -3,29 +3,54 @@ import unittest
 from indy_main import *
 
 class BinaryValueAddTests(unittest.TestCase):
+
 	@classmethod
 	def setUp(self) -> None:
 		self.binaryValue = BinaryValue('0')
 
+
 	def test_normal_add(self):
+		"""
+		Tests for normal binary
+		addition, including both
+		carry types.
+		"""
 		self.binaryValue.value = '0111'
 
 		res = self.binaryValue.add('11101011')
 		self.assertEqual(res, '011110010')
-	
+
+
 	def test_edge_case_add(self):
+		"""
+		Tests the function's ability
+		to add two single-bit binary
+		numbers.
+		"""
 		self.binaryValue.value = '0'
 
 		res = self.binaryValue.add('1')
 		self.assertEqual(res, '01')
-	
+
+
 	def test_edge_case_with_carry_add(self):
+		"""
+		Tests the function's ability
+		to carry correctly when adding
+		two single-bit binary numbers.
+		"""
 		self.binaryValue.value = '1'
 
 		res = self.binaryValue.add('1')
 		self.assertEqual(res, '010')
 
+
 	def test_tripple_one_carry_add(self):
+		"""
+		Tests the function's ability
+		to carry correctly when adding
+		three 1s.
+		"""
 		self.binaryValue.value = '11'
 
 		res = self.binaryValue.add('11')
@@ -33,6 +58,12 @@ class BinaryValueAddTests(unittest.TestCase):
 
 
 	def test_add_error(self):
+		"""
+		Tests the functions ability
+		to return the appropriate
+		error when supplied an invalid
+		operand.
+		"""
 		res = self.binaryValue.add('2121')
 		self.assertEqual(res, 'error: bad operand given')
 
