1f27f16d6484d1632b1e2e69b9cb3f0254afe3a9
Louis Stevens <louis@louisstevens.com>
2022-11-04 14:43:59 +0000

    -dp

--- gitup.py
+++ gitup.py
@@ -5,13 +5,38 @@ def main():
 
 	arguments = sys.argv
 	if len(arguments) == 1:
-		print('error: please provide a commit reason')
-		exit()
-	if len(arguments) > 2:
-		print(f'error: expected 1 argument, {len(arguments)} given')
-		exit()
+		print('error: please provide a commit reason (or command)')
+		exit(1)
+	elif arguments[1] == 'help':
+		print("""
+GitUp Help
 
-	os.system(f'git pull; git add --all; git commit -m "{arguments[1]}"; git push; git status')
+Normal Usage: py gitup.py "<commit message"
+Help Command: py gitup.py help
+
+
+Flags
+
+-dontpull, -dp	: Will not pull before attempting to commit and push
+-status, -s	: Will display status after attempting to push commit
+
+		""")
+		exit(0)
+	elif len(arguments) > 4:
+		print(f'error: expected max 3 argument, {len(arguments) - 1} given')
+		exit(1)
+	
+	dontPull = ('-dontpull' in arguments) or ('-dp' in arguments)
+	displayStatus = ('-status' in arguments) or ('-s' in arguments)
+	
+
+	os.system(f'{"" if dontPull else "git pull; "}git add --all; git commit -m "{arguments[1]}"; git push;')
+
+	print()
+
+	if displayStatus:
+		os.system("git status")
+
 
 
 if __name__ == '__main__':
