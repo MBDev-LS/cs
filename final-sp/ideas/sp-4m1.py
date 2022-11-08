0d488c5ca910d8547d0b1b3c94aab2b97f97233d
Louis <louis@louisstevens.com>
2022-11-08 09:19:16 +0000

    Update sp-4m1.py

--- final-sp/ideas/sp-4m1.py
+++ final-sp/ideas/sp-4m1.py
@@ -4,6 +4,7 @@
 # developed in the Python 3.9 programming environment
 
 # Converted indentation to tabs - Louis (04/11/2022)
+# Idea: Reject non-int inputs when getting square reference
 
 import random
 
